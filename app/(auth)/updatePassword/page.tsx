5de6400b2511af8c2cca0bd5502bdb8f1bd872a8
Keerthivasan K <keerthianbu2000@gmail.com>
2025-05-16 07:43:45 +0530

    Update page.tsx

--- app/(auth)/updatePassword/page.tsx
+++ app/(auth)/updatePassword/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { ResetForm } from "../components/ResetForm"
 
 
@@ -6,7 +7,9 @@ export default function updatePassword() {
         <>
             <div className="w-full h-[100vh] flex flex-col items-center justify-center p-10 bg-violet-700 dark:bg-[#1d1d1d]">
                 <div className="w-full border-1 border-white/60 dark:border-white/60 px-6 py-10 rounded-3xl md:w-2/3 lg:w-2xl bg-white dark:bg-[#1d1d1d] shadow-2xl">
-                    <ResetForm />
+                    <Suspense>
+                        <ResetForm />
+                    </Suspense>
                 </div>
             </div>
         </>
